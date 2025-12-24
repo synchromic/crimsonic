e671af90050ac0fb108dc3508d55c89618023315
synchromic <synchromic1@gmail.com>
2025-12-23 20:40:17 -0800

    add replay parsing

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,12 @@
 import json
+import os
 from datetime import timedelta
+from typing import cast
 
+import numpy as np
+import osrparse
 import slider
+from osrparse import KeyTaiko, Replay
 from slider import Beatmap
 
 ONE_MS = timedelta(milliseconds=1)
@@ -16,9 +21,8 @@ def is_kat(hit_object: slider.HitObject):
     return hit_object.hitsound & 0b1010 != 0
 
 
-def main():
+def parse_map():
     map = Beatmap.from_path("map.osu")
-
     # Assuming that the map has only one timing point
     timing_point = map.timing_points[0]
 
@@ -47,8 +51,131 @@ def main():
         "ms_per_beat": timing_point.ms_per_beat,
         "data": objects_str,
     }
-    with open("../website/src/assets/map.json", "w") as output_file:
+    path = "../website/src/assets/map.json"
+    with open(path, "w") as output_file:
+        print(f"Writing map to {path}")
         json.dump(json_output, output_file)
+
+
+def key_to_char(key):
+    match key:
+        case KeyTaiko.LEFT_DON:
+            return "d"
+        case KeyTaiko.LEFT_KAT:
+            return "k"
+        case KeyTaiko.RIGHT_DON:
+            return "D"
+        case KeyTaiko.RIGHT_KAT:
+            return "K"
+        case _:
+            raise ValueError(f"Invalid key: {key}")
+
+
+def parse_replay(path):
+    replay = Replay.from_path(f"replays/{path}")
+    if replay.mode != osrparse.GameMode.TAIKO:
+        raise ValueError(f"replay is not a taiko replay: {path}")
+    # print(replay.timestamp)
+
+    judgements = {
+        "300": replay.count_300,
+        "100": replay.count_100,
+        "miss": replay.count_miss,
+    }
+
+    events = []
+    cur_presses: dict[KeyTaiko, int | None] = {key: None for key in KeyTaiko}
+    data = cast(list[osrparse.ReplayEventTaiko], replay.replay_data)
+    time = 0
+    for ev in data:
+        time += ev.time_delta
+        for key in KeyTaiko:
+            pressed = (ev.keys & key) > 0
+            if pressed and cur_presses[key] is None:
+                cur_presses[key] = time
+            elif not pressed and cur_presses[key] is not None:
+                events.append(
+                    {
+                        "key": key,
+                        "press_time": cur_presses[key],
+                        "release_time": time,
+                    }
+                )
+                cur_presses[key] = None
+
+    # idk if there can be unreleased keys but just in case
+    time += 1000
+    for key in KeyTaiko:
+        if cur_presses[key] is not None:
+            events.append(
+                {
+                    "key": key,
+                    "press_time": cur_presses[key],
+                    "release_time": time,
+                }
+            )
+
+    # sort by increasing press time
+    events.sort(key=lambda ev: ev["press_time"])
+
+    # to have some kind of compression, we split hits into separate arrays for key, press time, release time
+    # keys: list of lowercase 'd', 'k' for left don/kat; uppercase 'D', 'K' for right don/kat
+    # we also convert times back into deltas (this almost halves the output size lol)
+    keys = "".join(key_to_char(ev["key"]) for ev in events)
+    presses = np.array([ev["press_time"] for ev in events], dtype=np.int32)
+    presses[1:] = np.ediff1d(presses)
+    releases = np.array([ev["release_time"] for ev in events], dtype=np.int32)
+    releases[1:] = np.ediff1d(releases)
+
+    return {
+        "date": replay.timestamp,
+        "judgements": judgements,
+        "keys": "".join(keys),
+        "press_time_deltas": presses,
+        "release_time_deltas": releases,
+    }
+
+
+def parse_replays():
+    replays = []
+    for path in os.listdir("replays"):
+        replays.append(parse_replay(path))
+
+    json_output = [
+        {
+            **replay,
+            "date": str(replay["date"]),
+            "press_time_deltas": replay["press_time_deltas"].tolist(),
+            "release_time_deltas": replay["release_time_deltas"].tolist(),
+        }
+        for replay in replays
+    ]
+    path = "../website/src/assets/replays.json"
+    with open(path, "w") as output_file:
+        print(f"Writing replays to {path}")
+        json.dump(json_output, output_file, separators=(",", ":"))
+
+
+def main():
+    print("crimsonic data parsing script")
+    print("enter 1 to parse map")
+    print("enter 2 to parse replays")
+    try:
+        selection = int(input("selection: "))
+    except ValueError:
+        print("invalid selection")
+        exit()
+    match selection:
+        case 1:
+            print("Parsing map...")
+            parse_map()
+            print("Done parsing map.")
+        case 2:
+            print("Parsing replays...")
+            parse_replays()
+            print("Done parsing replays.")
+        case _:
+            print("invalid selection")
 
 
 if __name__ == "__main__":
