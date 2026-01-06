cc312f8d92a9083f2bacbc5d135db137c5b4810e
synchromic <realvince7778@gmail.com>
2026-01-05 16:21:09 -0800

    remove picker and add replay titles

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
   import Scrubber from "./lib/Scrubber.svelte";
   import Audio from "./lib/Audio.svelte";
-  import { pickedReplay } from "./lib/pickerState.svelte";
   import { options } from "./lib/options.svelte";
   import ReplayContainer from "./lib/replay/ReplayContainer.svelte";
+
+  // TODO: get hovered replay to play hitsounds
 </script>
 
 <p>hello!</p>
-<Audio playingReplay={pickedReplay.replay ?? undefined} />
-<br />
-{#if pickedReplay.replay}
-  <div>
-    <label for="flyInput">Fly notes?</label>
-    <input type="checkbox" id="flyInput" bind:checked={options.flyNotes} />
-  </div>
-  <Scrubber />
-  <ReplayContainer />
-{/if}
+<Audio playingReplay={undefined} />
+<div>
+  <label for="flyInput">Fly notes?</label>
+  <input type="checkbox" id="flyInput" bind:checked={options.flyNotes} />
+</div>
+<Scrubber />
+<ReplayContainer />
--- website/src/lib/replay/ReplayRow.svelte
+++ website/src/lib/replay/ReplayRow.svelte
@@ -7,6 +7,23 @@
   let { replay, visible }: { replay: Replay; visible: boolean } = $props();
   let container: HTMLDivElement;
 
+  function formatDate(date: Date) {
+    return date.toLocaleString(undefined, {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+    });
+  }
+
+  function replayName(replay: Replay) {
+    const date = formatDate(new Date(replay.date));
+    const accuracy = (replay.accuracy * 100).toFixed(2);
+    const misses = replay.judgements.miss;
+    return `${date} (${accuracy}%, ${misses} misses)`;
+  }
+
   onMount(() => {
     addReplayRow({
       replay,
@@ -23,12 +40,13 @@
   });
 </script>
 
-<div bind:this={container}>
+<div id="outer" bind:this={container}>
+  <div>{replayName(replay)}</div>
   <Stats {replay} />
 </div>
 
 <style>
-  div {
+  #outer {
     min-height: 200px;
     border: 1px solid;
     padding: 5px;
