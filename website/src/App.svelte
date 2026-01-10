d334904a47e0e9c9405c92a9e80164cf74547bb6
synchromic <synchromic1@gmail.com>
2026-01-10 00:24:30 -0800

    play auto replay hitsounds

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -4,11 +4,15 @@
   import { options } from "./lib/options.svelte";
   import ReplayContainer from "./lib/replay/ReplayContainer.svelte";
 
+  import autoReplayJSON from "./assets/auto_replay.json";
+  import { Replay } from "./lib/replay/replay";
+
+  const autoReplay = new Replay(autoReplayJSON);
   // TODO: get hovered replay to play hitsounds
 </script>
 
 <p>hello!</p>
-<Audio playingReplay={undefined} />
+<Audio playingReplay={autoReplay} />
 <div>
   <label for="flyInput">Fly notes?</label>
   <input type="checkbox" id="flyInput" bind:checked={options.flyNotes} />
